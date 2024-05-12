0a5aa92cc0524dabe9f7a166c1011e175d637ca7
Abhishek Kapoor <42829419+Abhishek9111@users.noreply.github.com>
2024-05-12 08:32:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const jwtPassword = "123456";
+
+const app = express();
+
+const ALL_USERS = [
+  {
+    username: "harkirat@gmail.com",
+    password: "123",
+    name: "harkirat singh",
+  },
+  {
+    username: "raman@gmail.com",
+    password: "123321",
+    name: "Raman singh",
+  },
+  {
+    username: "priya@gmail.com",
+    password: "123321",
+    name: "Priya kumari",
+  },
+];
+app.use(express.json());
+
+function userExists(username, password) {
+  // write logic to return true or false if this user exists
+  // in ALL_USERS array
+  return true;
+}
+
+app.post("/signin", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!userExists(username, password)) {
+    return res.statusCode(403).json({
+      msg: "User doesn't exist",
+    });
+  }
+  var token = jwt.sign({ username: username }, jwtPassword);
+  return res.json({
+    token,
+  });
+});
+
+app.get("/users", (req, res) => {
+  const token = req.headers.authorization;
+
+  try {
+    const decoded = jwt.verify(token, jwtPassword);
+    const username = decoded.username;
+  } catch (err) {
+    return res.status(403).json({
+      msg: "Invalid token",
+    });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("server is now running");
+});
